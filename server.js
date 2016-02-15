90ae14bfc27d25c7c0b5fdf9ba126f0a2a5d5155
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-15 11:42:17 -0600

    mongo db setup for heroku and mongolab

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 const express = require('express');
 const app = express();  // or const app = require('express')();
 const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb://localhost:27017/node-webserver';
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
+
 const PORT = process.env.PORT || 3000; // eslint-disable-line no-magic-numbers
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017; // eslint-disable-line no-magic-numbers
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_NAME = process.env.MONGODB_NAME || 'node-webserver';
+const MONGODB_URL_PREFIX = MONGODB_USER
+	? `${MONGODB_USER}:${MONGODB_PASS}@`
+	: '';
+
+const MONGO_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
 
 const routes = require('./routes/');
 
